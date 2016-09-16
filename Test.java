8f2d4da40531974e1141ca284a3e9bec865b3bdc
Oscar Villavicencio <odvcencio@gmail.com>
2016-09-15 22:18:09 -0400

    First Commit

---
+++ Test.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+//This is my first time!!
+/*
+*/
+
+public class Test{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter an Integer: ");
+    int egg = sc.nextInt();
+
+    System.out.println("Hello World! " + kirby(egg));
+  }
+
+  public static int kirby(int x){
+    if(x == 0){
+      return x + 1000;
+    }else{
+      return x * 1000;
+    }
+  }
+}
